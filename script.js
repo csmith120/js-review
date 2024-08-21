f0c0d411c641644e9c37d4f29b8626201f280d41
CSmith3s <CSmith3s@capetigers.com>
2024-08-21 14:29:19 -0500

    mega man

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+/*
 let js = 'amazing';
-      if (js === 'amazing') alert('Javascript is fun');
+console.log(40 + 8 + 23 - 10);
 
-      40 + 8 + 23 - 10;
-      console.log(40 + 8 + 23 - 10);
+console.log('jonas');
+console.log(23);
+
+let firstName = "matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Varible name convention
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'none';
+let myCurrentJob = 'none';
+
+console.log(myFirstJob)
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
