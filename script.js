c41539f00b7274367b3dddfe4e2dae2b0ac29e39
CSmith3s <CSmith3s@capetigers.com>
2024-08-22 13:59:55 -0500

    reviewing down hill

--- script.js
+++ script.js
@@ -71,6 +71,19 @@ const firstName = 'C';
 const lastName = 'S'
 console.log(firstName + ' ' + lastName);
 
+// assginment oprators
 let x = 10 +5; //15
-x += 10; // x = x + 10
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x ++; // x = x + 1
+x --; // x = x - 1
+x --;
 console.log(x);
+
+//comparioson opratiers
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018)
