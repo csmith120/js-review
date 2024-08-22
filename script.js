f86c6d283104073850cb37e7ffa320a6a6b955b5
CSmith3s <CSmith3s@capetigers.com>
2024-08-22 13:44:50 -0500

    hope you are having a wonderful day

--- script.js
+++ script.js
@@ -59,6 +59,18 @@ lastName = 'CS';
 console.log(lastName);
 */
 
-const ageJonas = 2037 - 1991;
-const ageSarah = 2037 - 2018;
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'C';
+const lastName = 'S'
+console.log(firstName + ' ' + lastName);
+
+let x = 10 +5; //15
+x += 10; // x = x + 10
+console.log(x);
